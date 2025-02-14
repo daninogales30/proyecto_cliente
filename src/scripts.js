b51d6478eb8d11c96530f47e87a65b527ec301b4
Dario <dromdel1910@fp.iesromerovargas.com>
2025-02-14 09:15:12 +0100

    Terminado el index con imagenes y todo

--- src/scripts.js
+++ src/scripts.js
@@ -4,27 +4,37 @@ const slides = document.querySelectorAll('.slide');
 const totalSlides = slides.length;
 
 
+
+
 function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.classList.toggle('active', i === index);
-    });
+   slides.forEach((slide, i) => {
+       slide.classList.toggle('active', i === index);
+   });
 }
+
+
 
 
 function nextSlide() {
-    currentSlide = (currentSlide + 1) % totalSlides;
-    showSlide(currentSlide);
+   currentSlide = (currentSlide + 1) % totalSlides;
+   showSlide(currentSlide);
 }
+
+
 
 
 function prevSlide() {
-    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-    showSlide(currentSlide);
+   currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+   showSlide(currentSlide);
 }
+
+
 
 
 document.querySelector('.next').addEventListener('click', nextSlide);
 document.querySelector('.prev').addEventListener('click', prevSlide);
+
+
 
 
 // Carrusel de Opiniones
@@ -33,30 +43,41 @@ const testimonials = document.querySelectorAll('.testimonial');
 const totalTestimonials = testimonials.length;
 
 
+
+
 function showTestimonial(index) {
-    testimonials.forEach((testimonial, i) => {
-        testimonial.classList.toggle('active', i === index);
-    });
+   testimonials.forEach((testimonial, i) => {
+       testimonial.classList.toggle('active', i === index);
+   });
 }
+
+
 
 
 function nextTestimonial() {
-    currentTestimonial = (currentTestimonial + 1) % totalTestimonials;
-    showTestimonial(currentTestimonial);
+   currentTestimonial = (currentTestimonial + 1) % totalTestimonials;
+   showTestimonial(currentTestimonial);
 }
+
+
 
 
 function prevTestimonial() {
-    currentTestimonial = (currentTestimonial - 1 + totalTestimonials) % totalTestimonials;
-    showTestimonial(currentTestimonial);
+   currentTestimonial = (currentTestimonial - 1 + totalTestimonials) % totalTestimonials;
+   showTestimonial(currentTestimonial);
 }
+
+
 
 
 document.querySelector('.carousel-nav .next').addEventListener('click', nextTestimonial);
 document.querySelector('.carousel-nav .prev').addEventListener('click', prevTestimonial);
 
 
+
+
 // Cambiar slides automáticamente cada 5 segundos
 setInterval(nextSlide, 5000);
 setInterval(nextTestimonial, 5000);
+
 
